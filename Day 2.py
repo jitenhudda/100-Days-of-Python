86af40b00862598b43d6b8ce845ee17f65f5df2d
Jiten Hudda <jitenhudda45@gmail.com>
2026-06-05 16:21:09 +0530

    Add files via upload

---
+++ Day 2.py
@@ -0,0 +1,37 @@
+## DATA TYPES
+
+# 1 - Basic Data Types
+#integer-
+print(10)
+print("range of integer - ", 1e308)
+print(1e309)  # this shows infinity limit hit
+
+#float-
+print(4.5)
+
+#boolean-
+print(True)
+print(False)
+
+#complex-
+print(4+5j)
+
+#string-
+print('hello')
+print("hello")
+print("""hello""")
+
+
+# 2 - Container Data types
+
+#list - array of another language
+print([1,2,3,4,5])   # -- square bracket 
+
+#tuple - 
+print((1,2,3,45,5))  # -- simple bracket
+
+#sets - its a mathematical set
+print({1,3,4,2,6})   # -- curly bracket
+
+#dictonary (dict) - make a key value pair
+print({"name": "jiten" , "age":21, "gender":"male"})
