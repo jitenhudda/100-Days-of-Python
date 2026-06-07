28a3afdfb17ac89dd3f56cdb13ffbcc4f70e956b
Jiten Hudda <jitenhudda45@gmail.com>
2026-06-07 09:03:13 +0530

    Add files via upload

---
+++ Day 4.py
@@ -0,0 +1,35 @@
+# VARIABLE
+
+name = 'jiten'
+print(name)
+
+# in python do not use to declare a variable before using a variable
+
+#Feature of python--
+# Dynamic Typing - do not declaration a data types for variable (python use)
+# Static Typing - declare a data type (others language - c,c++ etc)
+
+print(name)
+
+name = 555
+print(name)
+# Dynamic Binding - one variable store multiple data types
+# ex- change name value - int, string anything and run give updated value
+
+# Static binding - if initialize the variable data types then you do not change the data types of that varibale in entire code 
+
+#special syntax--
+a = 2; b = 3; c = 6
+print(a)
+print(b)
+print(c)
+
+print(a,b,c)
+
+#also assign like this -
+a,b,c = 33,44,55
+print(a,b,c)
+
+#also one - 
+a=b=c = 10
+print(a,b,c)
