1932ba251d63b89ebfb76b14235003b1e4234492
Jiten Hudda <jitenhudda45@gmail.com>
2026-06-08 16:31:33 +0530

    Add files via upload

---
+++ Day 5.py
@@ -0,0 +1,8 @@
+# KEYWORDS
+
+#python is a case sensitive programing language
+#33 keywords
+# keywords -- reserved keyword like - true, false, break, class, import, etc
+
+import keyword
+print(keyword.kwlist)   #kwlist = keyword list
