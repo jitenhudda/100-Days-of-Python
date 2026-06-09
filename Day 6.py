da3d0829d852564bf1793bdc144626902ab206af
Jiten Hudda <jitenhudda45@gmail.com>
2026-06-09 17:21:00 +0530

    Add files via upload

---
+++ Day 6.py
@@ -0,0 +1,32 @@
+# IDENTIFIERS
+
+#you make a - object, variable, function, class - gives a name which is collectively tell indentifiers
+
+#Rules -
+#start with underscore and alphabet
+
+a = 'hello'
+print(a)
+_ = 'world'
+print(_)
+
+#do not asign a digit and special character gives error
+
+#2 = 'hello'
+#print(2)    
+ 
+# gives this error -  File "c:\Users\HP\Desktop\100 days Python\Day 6.py", line 15
+#    2 = 'hello'
+#    ^
+#SyntaxError: cannot assign to literal here. Maybe you meant '==' instead of '='?
+#PS C:\Users\HP\Desktop\100 days Python> python -u "c:\Users\HP\Desktop\100 days Python\Day 6.py"
+#  File "c:\Users\HP\Desktop\100 days Python\Day 6.py", line 15
+#    2 = 'hello'
+#    ^
+#SyntaxError: cannot assign to literal here. Maybe you meant '==' instead of '='?
+#PS C:\Users\HP\Desktop\100 days Python> 
+
+first_name = 'rahul'
+print(first_name)
+
+# keywords is not a identifiers name
