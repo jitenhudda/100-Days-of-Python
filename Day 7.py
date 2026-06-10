660f5c971e32e23b8b861c7285bba78cacc68036
Jiten Hudda <jitenhudda45@gmail.com>
2026-06-10 17:07:37 +0530

    Add files via upload

---
+++ Day 7.py
@@ -0,0 +1,30 @@
+# Taking User Input
+
+#two types of software - 
+#1- static - only read like- govt site
+#2- dynamic - read and give input and output like - zomato, uber
+
+#input()  ---- use like this in coding 
+#but what i type - name, value so give a proper 
+
+input('apna naam bta -')
+
+
+first_num = input('Enter first number - ')
+second_num = input('enter second number - ')
+print(first_num)
+print(second_num)
+
+#result = first_num + second_num
+#print(result) 
+
+#output---
+#Enter first number - 4
+#enter second number - 5
+#4
+#5
+#45    -- input going at a string format
+
+#use type function -- to check the data type of variable 
+type(first_num)
+print(type(first_num))
