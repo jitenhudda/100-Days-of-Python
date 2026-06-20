7608767335c77c74c45334694082c2e7b0206eae
Jiten Hudda <jitenhudda45@gmail.com>
2026-06-20 06:53:17 +0530

    Delete Day 5.py

--- Day 5.py
+++
@@ -1,8 +0,0 @@
-# KEYWORDS
-
-#python is a case sensitive programing language
-#33 keywords
-# keywords -- reserved keyword like - true, false, break, class, import, etc
-
-import keyword
-print(keyword.kwlist)   #kwlist = keyword list
