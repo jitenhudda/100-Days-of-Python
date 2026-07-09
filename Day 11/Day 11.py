e8395b6683c521698c776b7f599e84f03bb16a0b
Jiten Hudda <jitenhudda45@gmail.com>
2026-07-09 09:18:53 +0530

    Add files via upload

---
+++ Day 11/Day 11.py
@@ -0,0 +1,14 @@
+# While loop
+
+#syntax -
+#while condition:
+#   code
+
+#table print of any number
+
+number = int(input('Enter your number - '))
+
+i = 1
+while i<11:
+    print(number, "*", i, "=", number*i)
+    i += 1
